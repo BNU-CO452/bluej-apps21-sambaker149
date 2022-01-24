5e860e9931929cd4a08909c6cc5fc7c720fb231c
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2022-01-24 14:52:54 +0000

    Copied code into BlueJ project

---
+++ app216-zuul/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    /**
+     * Create a new game and start to play it.
+     * @author Samuel Baker
+     * @version 03/12/2021
+     */
+    public static void main(String[] args)
+    {
+        Game zuul = new Game();
+        zuul.play();
+    }
+}
